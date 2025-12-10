e3c907b15f2b0f69180919aad82e01e666bdef67
kirtan Limbachiya <108492403+kirtan-226@users.noreply.github.com>
2025-12-11 00:43:20 +0100

    on it

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
 const app = require('../src/app');
 
 // Vercel serverless entrypoint: forward requests to Express so middleware (incl. CORS) runs
-module.exports = (req, res) => app(req, res);
+module.exports = (req, res) => app(req, res); 
